ad378c70e141d526957597c03fab0f70f9790a81
borianyi serrano <borianyiserrano@gmail.com>
2019-05-15 20:06:29 -0400

    ultima clase

---
+++ bootstrap-4.3.1-dist/js/perfil2.js
@@ -0,0 +1,13 @@
+
+  $( document ).ready(function() {
+    console.log( "ready!" );
+
+    $('.carousel').carousel({
+        interval: 2000,
+        pause: false, 
+        Wrap: false,
+      })
+
+
+
+})
